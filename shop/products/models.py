5bae61c332d2641029eb9e69c418701116874983
Alexander Chaika <manti.by@gmail.com>
2023-03-01 19:06:06 +0300

    Add purchase and product models

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="purchases"
+    )
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.CASCADE, related_name="purchases"
+    )
+    count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
