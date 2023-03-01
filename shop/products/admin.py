f345c607c8e29ea398a47bb28400feee8e6df668
Alexander Chaika <manti.by@gmail.com>
2023-03-01 22:01:56 +0300

    Add classwork

--- shop/products/admin.py
+++ shop/products/admin.py
@@ -9,8 +9,8 @@ class PurchaseAdminInline(admin.StackedInline):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("title", "price", "description", "created_at")
-    fields = ("title", "price", "description", "created_at")
+    list_display = ("title", "price", "color", "description", "created_at")
+    fields = ("title", "price", "color", "description", "created_at")
     readonly_fields = ("created_at",)
     search_fields = ("title", "description")
     inlines = (PurchaseAdminInline,)
--- shop/products/models.py
+++ shop/products/models.py
@@ -2,8 +2,16 @@ from django.conf import settings
 from django.db import models
 
 
+COLOR_CHOICES = (
+    ("RED", "Red"),
+    ("GREEN", "Green"),
+    ("BLUE", "Red"),
+)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    color = models.CharField(max_length=32, choices=COLOR_CHOICES, default="RED")
     price = models.IntegerField(default=0)
     excerpt = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
