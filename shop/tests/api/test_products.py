9448914033f0cfa7339cdbba0d28b3409692cd92
Alexander Chaika <manti.by@gmail.com>
2023-05-06 17:05:33 +0300

    Add product APIs, add tests and factories

--- shop/tests/api/test_products.py
+++ shop/tests/api/test_products.py
@@ -2,6 +2,7 @@ import pytest
 from rest_framework.test import APIClient
 
 from products.models import Product
+from tests.factories import ProductFactory
 
 
 @pytest.mark.django_db
@@ -32,3 +33,10 @@ class TestProductsApi:
         response = self.client.delete(f"/api/products/{product.id}/")
         assert response.status_code == 204
         assert not Product.objects.exists()
+
+    def test_popular(self):
+        ProductFactory.create_batch(10)
+        response = self.client.get("/api/products/popular/")
+
+        assert response.status_code == 200
+        assert response.json().get("count") == 10
