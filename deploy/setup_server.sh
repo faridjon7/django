23fc2849d8b989588084377dbc02425ebe45f9a8
Alexander Chaika <manti.by@gmail.com>
2023-06-05 21:18:39 +0300

    Add linters support

--- deploy/setup_server.sh
+++ deploy/setup_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo ufw disable
 sudo apt install nginx
-sudo apt install -y postgresql postgresql-contrib
+sudo apt install -y postgresql postgresql-contrib redis-server python3-virtualenv
 sudo apt install redis-server
 sudo apt install python3-virtualenv
